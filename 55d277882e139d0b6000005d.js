9f88a93928e82cba1937f0555c5e334aff0ba0f4
Baz Murphy <bazmurphy@gmail.com>
2022-05-03 10:15:23 +0100

    03-05-2022

---
+++ 55d277882e139d0b6000005d.js
@@ -0,0 +1,35 @@
+// Grasshopper - Array Mean
+
+// Find Mean
+// Find the mean (average) of a list of numbers in an array.
+// Information
+// To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+// For an example list of 1, 3, 5, 7
+// 1. Add all of the numbers
+// 1+3+5+7 = 16
+// 2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+// 16/4 = 4
+// 3. The mean (or average) of this list is 4
+
+var findAverage = function (nums) {
+    return nums.reduce( (element, currentValue) => element + currentValue) / nums.length;
+}
+
+
+// Solution 1
+const findAverage = nums => nums.reduce((a, b) => a + b) / nums.length;
+
+// Solution 2
+var findAverage = function(nums){
+    return nums.reduce((a, b) => a + b, 0) / nums.length;
+}
+
+// Solution 3
+var findAverage = function (nums) {
+    // Code here
+    var sum = 0;
+    for (num in nums) {
+      sum += nums[num];
+    }
+    return sum / (nums.length);
+}
