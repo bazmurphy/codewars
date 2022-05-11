05b7b222fff51ffc1e1bf502948c36a8598cacd8
Baz Murphy <bazmurphy@gmail.com>
2022-05-11 08:25:53 +0100

    11-05-2022

---
+++ 8kyu/53dc54212259ed3d4f00071c.js
@@ -0,0 +1,76 @@
+// Sum Arrays
+
+// Write a function that takes an array of numbers and returns the sum of the numbers. 
+// The numbers can be negative or non-integer. 
+// If the array does not contain any numbers then you should return 0.
+
+// Examples
+
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Assumptions
+//     You can assume that you are only given numbers.
+//     You cannot assume the size of the array.
+//     You can assume that you do get an array and if the array is empty, return 0.
+
+// What We're Testing
+
+// We're testing basic loops and math operations. 
+// This is for beginners who are just learning loops and math operations.
+// Advanced users may find this extremely easy and can easily write this in one line.
+
+
+// Sum Numbers
+function sum (numbers) {
+
+    return numbers.reduce( (accumulator, current) => accumulator + current, 0)
+
+}
+
+
+// Solution 1
+function sum(numbers) {
+    return numbers.reduce((a, b) => a + b, 0);
+}
+
+
+// Solution 2
+sum = function (numbers) {
+    "use strict";
+    return numbers.reduce(function(t, n){
+      return t + n;
+    }, 0);
+  };
+
+
+// Solution 3
+sum = function (numbers) {
+    "use strict";
+    var total = 0;
+    for(var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    return total;
+  };
+
+
+// Solution 4
+const sum = n => n.reduce((a,b) => a+b, 0);
+
+
+// Solution 5
+sum = function (numbers) {
+    "use strict";
+    return numbers.reduce(function(prev, cur) { return prev + cur }, 0);
+};
+
+
+// Solution 6
+const sum = (a) => a.reduce((s,e) => s += e, 0);
