27cb7bfabbc7d25a45ab682d7bd9032a8d45a4e4
Baz Murphy <bazmurphy@gmail.com>
2022-08-19 08:41:55 +0000

    19-08-2022

---
+++ 8kyu/59441520102eaa25260000bf.js
@@ -0,0 +1,56 @@
+// 5 without numbers !!
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+    return "silly".length;
+}
+
+console.log(unusualFive());
+// 5
+
+
+
+// Solution 1
+function unusualFive() {
+  return 'fucku'.length
+}
+
+
+// Solution 2
+const unusualFive = () => ['Putin', 'huilo', 'lalala', 'lalala', 'lalala'].length
+
+
+// Solution 3
+function unusualFive() {
+    return 'DaniQ'.length
+}
+
+
+// Solution 4
+function unusualFive() {
+  return "jesus".length;
+}
+
+
+// Solution 7
+const unusualFive = () =>
+  Math.hypot(Math.ceil(Math.PI), Math.floor(Math.PI));
+
+
+// Solution 8
+unusualFive=_=>'     '.length
+
+
+// Solution 9
+const unusualFive = () => 'How do you do bro?'.split(' ').length
+
+
+// Solution 11
+function unusualFive() {
+    return new Date().getMinutes()
+}
