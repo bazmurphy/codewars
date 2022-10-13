287862b775aea484bd8c2e6ddb85c961170cdc27
Baz Murphy <bazmurphy@gmail.com>
2022-10-13 08:39:19 +0100

    13-10-2022

---
+++ 8kyu/523b4ff7adca849afe000035.js
@@ -0,0 +1,26 @@
+// Function 1 - hello world
+
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+function greet() {
+  return "hello world!";
+}
+
+console.log(typeof greet); 
+// "function"
+console.log(greet());
+// "hello world!"
+
+
+// Solution 1
+function greet() {
+  return "hello world!";
+}
+
+
+// Solution 2
+const greet = () => "hello world!";
