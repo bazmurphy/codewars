2c52b57596d04a95916b34f3166f9ed41a8a633f
Baz <61154071+bazmurphy@users.noreply.github.com>
2022-12-18 08:11:56 +0000

    18-12-2022

---
+++ 7kyu/532b4057484b0e58e8000766.js
@@ -0,0 +1,25 @@
+// Use reduce() to calculate the sum of the values in an array (retired)
+
+// Make the sum() function return the sum of the values in the passed in array. Your solution must use the reduce() function of the built-in javascript Array object. If you're not familiar with reduce(), reading over the documentation may help.
+
+// function sum(array) {
+//   // Use array.reduce() to find and return the
+//   // sum of the values in array.
+// }
+
+// For example:
+
+// var someNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+// sum(someNumbers); // should return 55
+
+function sum(array) {
+    return array.reduce((pV, cV) => pV + cV, 0)
+}
+
+console.log(sum([1,2,3,4,5,6,7,8,9,10]));
+// 55
+console.log(sum([71,-548,12.3,83,-47,-1.7,-892,58,31415,2718,-38]));
+// 32830.6
+
+
