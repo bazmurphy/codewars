adc0760f56078c1c2e9a01b3ea4b4be23b290839
bazmurphy <bazmurphy@gmail.com>
2023-03-09 08:23:39 +0000

    09-03-2023

---
+++ 7kyu/56eb0be52caf798c630013c0.js
@@ -0,0 +1,49 @@
+// Unlucky Days
+
+// Friday 13th or Black Friday is considered as unlucky day. 
+// Calculate how many unlucky days are in the given year.
+
+// Find the number of Friday 13th in the given year.
+
+// Input: Year in Gregorian calendar as integer.
+
+// Output: Number of Black Fridays in the year as an integer.
+
+// Examples:
+
+// unluckyDays(2015) == 3
+// unluckyDays(1986) == 1
+
+function unluckyDays(year){
+
+  // const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+  // let then  = new Date(year, month, day);
+  // console.log(then);
+  // console.log(`${weekdays[then.getDay()]} ${then.getDay()}`);
+
+  // let day = new Date("Jan 01 1586 00:00:00")
+  // console.log(day)
+
+}
+
+console.log(unluckyDays(1586));
+// 1
+console.log(unluckyDays(1001));
+// 3
+console.log(unluckyDays(2819));
+// 2
+console.log(unluckyDays(2792));
+// 2
+console.log(unluckyDays(2723));
+// 2
+console.log(unluckyDays(1909));
+// 1
+console.log(unluckyDays(1812));
+// 2
+console.log(unluckyDays(1618));
+// 2
+console.log(unluckyDays(2132));
+// 1
+console.log(unluckyDays(2065));
+// 3
